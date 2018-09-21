41a5aeb3fcfa69441bd166b671712014004dbc94
Elvis Porebski <eporebski@zendesk.com>
2018-09-21 13:44:45 +0100

    Add sample app for Answer Bot SDK

---
+++ answerbot_sample/src/main/java/com/zendesk/answerbot_sample/Global.java
@@ -0,0 +1,51 @@
+package com.zendesk.answerbot_sample;
+
+import android.app.Application;
+
+import com.zendesk.logger.Logger;
+
+import zendesk.answerbot.AnswerBot;
+import zendesk.core.AnonymousIdentity;
+import zendesk.core.Zendesk;
+import zendesk.support.Support;
+
+public class Global extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable logging
+        Logger.setLoggable(true);
+
+        /*
+          Initialize the SDK with your Zendesk subdomain, mobile SDK app ID, and client ID.
+
+          Get these details from your Zendesk dashboard: Admin -> Channels -> MobileSDK.
+         */
+        Zendesk.INSTANCE.init(this,
+                "https://{subdomain}.zendesk.com",
+                "{applicationId}",
+                "{oauthClientId}");
+
+        /*
+          Set an identity (authentication).
+
+          Set either Anonymous or JWT identity, as below:
+         */
+
+        // a). Anonymous (All fields are optional)
+        Zendesk.INSTANCE.setIdentity(
+                new AnonymousIdentity.Builder()
+                        .withNameIdentifier("{optional name}")
+                        .withEmailIdentifier("{optional email}")
+                        .build()
+        );
+
+        // b). JWT (Must be initialized with your JWT identifier)
+        // Zendesk.INSTANCE.setIdentity(new JwtIdentity("{JWT User Identifier}"));
+
+        Support.INSTANCE.init(Zendesk.INSTANCE);
+        AnswerBot.INSTANCE.init(Zendesk.INSTANCE, Support.INSTANCE);
+    }
+}
