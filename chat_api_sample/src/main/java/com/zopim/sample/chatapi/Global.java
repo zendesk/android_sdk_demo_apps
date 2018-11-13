5a4d1996c47732522fdf96bb7fafc0328ff2f89d
danthedrummer <danthedrummerrof@gmail.com>
2018-11-13 13:15:47 +0000

    Moving Firebase init into Global

--- chat_api_sample/src/main/java/com/zopim/sample/chatapi/Global.java
+++ chat_api_sample/src/main/java/com/zopim/sample/chatapi/Global.java
@@ -1,12 +1,18 @@
 package com.zopim.sample.chatapi;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.zendesk.logger.Logger;
 import com.zendesk.util.StringUtils;
 import com.zopim.android.sdk.api.ZopimChatApi;
 
 public class Global extends Application {
+
+    private final static String LOG_TAG = "ChatApiSample";
 
     /**
      * Account config needed to initialize {@link com.zopim.android.sdk.api.ZopimChatApi#init(String)}
@@ -14,6 +20,9 @@ public class Global extends Application {
      * Account key can be found in Zopim Dashboard at the <a href="https://dashboard.zopim.com/#widget/getting_started">Getting Started Page</a>
      */
     private final static String ACCOUNT_KEY = ""; // NB: Replace this key with your Zopim account key
+    private final static String PROJECT_ID = ""; // Firebase console -> Project settings -> General -> Project Id
+    private final static String API_KEY = ""; // Firebase console -> Project settings -> Cloud messaging -> Server Key
+    private final static String FCM_SENDER_ID = ""; // Firebase console -> Project settings -> Cloud messaging -> Sender ID
 
     @Override
     public void onCreate() {
@@ -30,6 +39,27 @@ public class Global extends Application {
         ZopimChatApi.init(ACCOUNT_KEY);
 
         // Initialise Firebase app to receive push notifications
-        FirebasePushSetup.initPush(this);
+        if (PROJECT_ID.isEmpty() || API_KEY.isEmpty() || FCM_SENDER_ID.isEmpty()) {
+            throw new RuntimeException("To use push in the sample app you must configure " +
+                    "the credentials");
+        }
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setApplicationId(PROJECT_ID)
+                .setApiKey(API_KEY)
+                .setGcmSenderId(FCM_SENDER_ID)
+                .build();
+
+        FirebaseApp.initializeApp(this, options);
+
+        try {
+            String token = FirebaseInstanceId.getInstance().getToken();
+            if (token != null) {
+                Log.d(LOG_TAG, "Obtained FCM token");
+                ZopimChatApi.setPushToken(token);
+            }
+        } catch (IllegalStateException e) {
+            Log.d(LOG_TAG, "Error requesting FCM token");
+        }
     }
 }
--- chat_sample/src/main/java/com/zopim/sample/Global.java
+++ chat_sample/src/main/java/com/zopim/sample/Global.java
@@ -3,6 +3,9 @@ package com.zopim.sample;
 import android.app.Application;
 import android.util.Log;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.zendesk.logger.Logger;
 import com.zendesk.util.StringUtils;
 import com.zopim.android.sdk.api.ZopimChat;
@@ -10,12 +13,17 @@ import com.zopim.android.sdk.model.VisitorInfo;
 
 public class Global extends Application {
 
+    private final static String LOG_TAG = "ChatSample";
+
     /**
      * Account config needed to initialize {@link com.zopim.android.sdk.api.ZopimChatApi#init(String)}
      * <p/>
      * Account key can be found in Zopim Dashboard at the <a href="https://dashboard.zopim.com/#widget/getting_started">Getting Started Page</a>
      */
     private final static String ACCOUNT_KEY = ""; // NB: Replace this key with your Zopim account key
+    private final static String PROJECT_ID = ""; // Firebase console -> Project settings -> General -> Project Id
+    private final static String API_KEY = ""; // Firebase console -> Project settings -> Cloud messaging -> Server Key
+    private final static String FCM_SENDER_ID = ""; // Firebase console -> Project settings -> Cloud messaging -> Sender ID
 
     @Override
     public void onCreate() {
@@ -69,7 +77,28 @@ public class Global extends Application {
         }
 
         // Initialise Firebase app to receive push notifications
-        FirebasePushSetup.initPush(this);
+        if (PROJECT_ID.isEmpty() || API_KEY.isEmpty() || FCM_SENDER_ID.isEmpty()) {
+            throw new RuntimeException("To use push in the sample app you must configure " +
+                    "the credentials");
+        }
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setApplicationId(PROJECT_ID)
+                .setApiKey(API_KEY)
+                .setGcmSenderId(FCM_SENDER_ID)
+                .build();
+
+        FirebaseApp.initializeApp(this, options);
+
+        try {
+            String token = FirebaseInstanceId.getInstance().getToken();
+            if (token != null) {
+                Log.d(LOG_TAG, "Obtained FCM token");
+                ZopimChat.setPushToken(token);
+            }
+        } catch (IllegalStateException e) {
+            Log.d(LOG_TAG, "Error requesting FCM token");
+        }
 
         // clear visitor info. Visitor info storage can be disabled at chat initialization
         VisitorInfo emptyVisitorInfo = new VisitorInfo.Builder().build();
