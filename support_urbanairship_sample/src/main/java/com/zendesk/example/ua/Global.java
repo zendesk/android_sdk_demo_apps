c6834e0a818cef8d328e1f97fd3e61645f498df7
Brendan Fahy <bfahy@zendesk.com>
2019-03-19 15:34:47 +0000

    Update to Support SDK 2.3.0 + a few tweaks

--- support_urbanairship_sample/src/main/java/com/zendesk/example/ua/Global.java
+++ support_urbanairship_sample/src/main/java/com/zendesk/example/ua/Global.java
@@ -8,7 +8,6 @@ import com.zendesk.logger.Logger;
 import com.zendesk.util.StringUtils;
 
 import zendesk.core.AnonymousIdentity;
-import zendesk.core.Identity;
 import zendesk.core.Zendesk;
 import zendesk.support.Support;
 
@@ -33,12 +32,10 @@ public class Global extends Application {
             return;
         }
 
-        Zendesk.INSTANCE.init(
-                this,
-                getString(R.string.zd_url),
-                getString(R.string.zd_appid),
-                getString(R.string.zd_oauth)
-        );
+        Zendesk.INSTANCE.init(this,
+                SUBDOMAIN_URL,
+                APPLICATION_ID,
+                OAUTH_CLIENT_ID);
 
         Zendesk.INSTANCE.setIdentity(
                 new AnonymousIdentity.Builder()
