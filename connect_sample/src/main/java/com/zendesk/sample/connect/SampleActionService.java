c0e004a588d68a16a09d1c697e645fbd4f904bc2
Filipe Belatti <fibelatti@gmail.com>
2019-08-02 08:54:06 +0100

    Update connect demo to v3.0.0

--- connect_sample/src/main/java/com/zendesk/sample/connect/SampleActionService.java
+++ connect_sample/src/main/java/com/zendesk/sample/connect/SampleActionService.java
@@ -4,14 +4,14 @@ import android.content.Intent;
 import android.net.Uri;
 
 import com.zendesk.connect.ConnectActionService;
-import com.zendesk.connect.NotificationPayload;
+import com.zendesk.connect.SystemPushPayload;
 import com.zendesk.logger.Logger;
 
 /**
  * You can optionally extend {@link ConnectActionService} to handle what happens when
  * a user taps on a push notification delivered by Connect. This service must be
  * registered in your manifest.
- *
+ * <p>
  * If you don't want to handle push actions yourself then you can leave out this
  * class and register {@link ConnectActionService} in your manifest.
  */
@@ -20,9 +20,10 @@ public class SampleActionService extends ConnectActionService {
     private final static String LOG_TAG = "SampleActionService";
 
     @Override
-    public void onOpenNotification(NotificationPayload payload) {
-        Logger.d(LOG_TAG, "User opened notification and was directed to: "
-                + payload.getDeeplinkUrl());
+    public void onOpenNotification(SystemPushPayload payload) {
+        Logger.d(LOG_TAG,
+                "User opened notification and was directed to: %s",
+                payload.getDeeplinkUrl());
 
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(payload.getDeeplinkUrl()));
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
