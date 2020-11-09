af844dbab7c4f73d544978b6e509f33058446faa
jakubbialkowski <jbialkowski@zendesk.com>
2020-11-09 09:00:58 +0100

    Basic UI

--- talk_sample/src/main/java/com/zendesk/talk/sample/MainActivity.kt
+++ talk_sample/src/main/java/com/zendesk/talk/sample/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.zendesk.talk.sample
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.zendesk.talk.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_activity_menu, menu)
+        return true
     }
 }
