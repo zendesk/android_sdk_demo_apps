e4060d2061467336763d6666066a7ced056fed3e
jakubbialkowski <jbialkowski@zendesk.com>
2020-11-09 09:10:31 +0100

    Handling missing credentials

--- talk_sample/src/main/java/com/zendesk/talk/sample/MainActivity.kt
+++ talk_sample/src/main/java/com/zendesk/talk/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zendesk.talk.sample
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.zendesk.talk.sample.databinding.ActivityMainBinding
 
@@ -13,10 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.setMissingCredentials(SampleApplication.isMissingCredentials())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_activity_menu, menu)
-        return true
+        if (!SampleApplication.isMissingCredentials()) {
+            menuInflater.inflate(R.menu.main_activity_menu, menu)
+            return true
+        }
+        return false
+    }
+
+    private fun ActivityMainBinding.setMissingCredentials(missing: Boolean) {
+        val views = listOf(chatBubble, line1, queuePosition, line2, messageDivider, chatForm, send)
+        val viewsVisibility = if (missing) View.GONE else View.VISIBLE
+        views.forEach { view -> view.visibility = viewsVisibility }
+        missingCredentials.visibility = if (missing) View.VISIBLE else View.GONE
     }
 }
